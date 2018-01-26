e75bdbd908a7bd3e9a3a30292e2a91b55d254b58
Mars Wong <marswong618@gmail.com>
2018-01-26 02:59:14 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,19 @@
 require 'pry'
 
 def my_find(collection)
-
+  if block_given?
+    i = 0
+    result = nil
+    while i < collection.length
+      if yield(collection[i])
+        result = collection[i]
+        break
+      else
+        i += 1        
+      end
+    end
+    result
+  else
+    throw SyntaxError
+  end
 end
