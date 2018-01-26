5d80dff3057c8cb92a92f2f89ec02c3dfc5df80b
Mars Wong <marswong618@gmail.com>
2018-01-26 02:57:50 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,7 +2,11 @@ require 'pry'
 
 def my_find(collection)
   if block_given?
-    
+    i = 0
+    result = 0
+    while i < collection.length
+      i += 1
+    end
   else
     throw SyntaxError
   end
